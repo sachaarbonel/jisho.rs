9b68b13681f6029c16df2cce736bee5612b35c3a
Sach97 <sacha.arbonel@hotmail.fr>
2019-10-08 03:01:19 +0200

    dynamic query and index path

--- example/jisho_example.dart
+++ example/jisho_example.dart
@@ -1,14 +1,15 @@
 import 'dart:ffi' as ffi;
 import 'package:ffi/ffi.dart';
 
-typedef NativeRustPlayOnceFunction = ffi.Pointer<Utf8> Function();
+typedef NativeRustPlayOnceFunction = ffi.Pointer<Utf8> Function(
+    ffi.Pointer<Utf8>, ffi.Pointer<Utf8>);
 
 main() {
   ffi.DynamicLibrary dl = ffi.DynamicLibrary.open("target/debug/libjisho.so");
   var search =
       dl.lookupFunction<NativeRustPlayOnceFunction, NativeRustPlayOnceFunction>(
           "search");
-  // final Pointer<Utf8> index = Utf8.toUtf8("./index").cast();
-  // final Pointer<Utf8> query = Utf8.toUtf8("人間").cast();
-  print(Utf8.fromUtf8(search())); //query, index
+  final ffi.Pointer<Utf8> index = Utf8.toUtf8("./index").cast();
+  final ffi.Pointer<Utf8> query = Utf8.toUtf8("人間").cast();
+  print(Utf8.fromUtf8(search(index, query))); //query, index
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 mod search;
+use std::ffi::CStr;
 use std::ffi::CString;
 use std::os::raw::c_char;
 use std::path::Path;
 
 #[no_mangle]
-pub extern "C" fn search() -> *const c_char {
-    // let pathstr = unsafe { CStr::from_ptr(path_ptr) };
-    // let querystr = unsafe { CStr::from_ptr(query_ptr) };
-    // let directory = Path::new(pathstr.to_str().unwrap());
-    // let query = querystr.to_str().unwrap();
-    //println!("{:?}", search::search_document(&directory, query));
-    let v = search::search_document(Path::new("./index"), "人間");
+pub extern "C" fn search(index_ptr: *const c_char, query_ptr: *const c_char) -> *const c_char {
+    let indexstr = unsafe { CStr::from_ptr(index_ptr) };
+    let querystr = unsafe { CStr::from_ptr(query_ptr) };
+    let index = Path::new(indexstr.to_str().unwrap());
+    let query = querystr.to_str().unwrap();
+    let v = search::search_document(index, query);
     let s = CString::new(v).unwrap();
     let p = s.as_ptr();
     std::mem::forget(s);
