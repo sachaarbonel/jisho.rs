6b2459105d29d57def73326628039f01758db9cf
Sach97 <sacha.arbonel@hotmail.fr>
2019-10-08 14:14:56 +0200

    rollback

--- lib/jisho.dart
+++ lib/jisho.dart
@@ -1,13 +1,1 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class Jisho {
-  static const MethodChannel _channel =
-      const MethodChannel('jisho');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/jisho_base.dart';
